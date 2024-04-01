c19db29a3f96064e17a3370a2fbdb3813ea4e74e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-04-02 00:39:46 +0330

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,31 @@
 const { JSDOM } = require("jsdom");
 
+async function crawlPage(currentURL) {
+  console.log(`actively crawling: ${currentURL}`);
+  try {
+    const resp = await fetch(currentURL);
+
+    if (resp.status > 399) {
+      console.log(
+        `error in fetch with status code: ${resp.status} on page: ${currentURL}`
+      );
+      return;
+    }
+
+    const contentType = resp.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `non html response, content type: ${contentType}, on page: ${currentURL}`
+      );
+      return;
+    }
+
+    console.log(await resp.text());
+  } catch (err) {
+    console.log(`error in fetch: ${err.message}, on page: ${currentURL}`);
+  }
+}
+
 function getURLsFormHTML(htmlBody, baseURL) {
   const urls = [];
   const dom = new JSDOM(htmlBody);
@@ -26,6 +52,7 @@ function getURLsFormHTML(htmlBody, baseURL) {
 
   return urls;
 }
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -36,6 +63,7 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
+  crawlPage,
   normalizeURL,
   getURLsFormHTML,
 };
